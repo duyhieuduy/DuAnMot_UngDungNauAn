f41e9e21b376846c30d78071eb2341fa69bccd14
duyhieuduy <hieu01648476512@gmail.com>
2022-11-06 10:55:29 +0700

    Update OTP.java

--- App_Nau_An/app/src/main/java/com/example/cookingapp/OTP.java
+++ App_Nau_An/app/src/main/java/com/example/cookingapp/OTP.java
@@ -1,6 +1,6 @@
 package com.example.cookingapp;
 
-import static com.example.cookingapp.R.id.verifyBtn;
+import  com.example.cookingapp.R.id.verifyBtn;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
