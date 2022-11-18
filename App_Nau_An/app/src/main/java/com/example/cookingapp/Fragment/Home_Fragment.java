938f44950ea4121a94c31a6fd11cfca563f3bdfc
phong <phonghack02041999@gmail.com>
2022-11-18 19:38:33 +0700

    v.v...

--- App_Nau_An/app/src/main/java/com/example/cookingapp/Fragment/Home_Fragment.java
+++ App_Nau_An/app/src/main/java/com/example/cookingapp/Fragment/Home_Fragment.java
@@ -1,33 +1,19 @@
 package com.example.cookingapp.Fragment;
 
-import static android.content.Context.MODE_PRIVATE;
-
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.ListView;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.cookingapp.Adapter.LoaiMonAdapter;
-import com.example.cookingapp.Adapter.MonAnAdapter;
 import com.example.cookingapp.R;
-import com.example.cookingapp.dao.LoaiMonDAO;
-import com.example.cookingapp.dao.MonAnDAO;
 import com.example.cookingapp.model.LoaiMon;
-import com.example.cookingapp.model.MonAn;
 
 import java.util.ArrayList;
 
@@ -38,18 +24,6 @@ public class Home_Fragment extends Fragment {
         View view = inflater.inflate(R.layout.home_fragment, container, false);
 
         RecyclerView recyclerMonAn = view.findViewById(R.id.recyclerMonAn);
-
-        LoaiMonDAO loaiMonDAO = new LoaiMonDAO(getContext());
-
-
-        ArrayList<LoaiMon> list = loaiMonDAO.getDSLoaiMon();
-
-
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
-        recyclerMonAn.setLayoutManager(linearLayoutManager);
-        LoaiMonAdapter adapter = new LoaiMonAdapter(getContext(), list);
-        recyclerMonAn.setAdapter(adapter);
-
 
 
         return view;
--- App_Nau_An/app/src/main/java/com/example/cookingapp/ThemMonMoiActivity.java
+++ App_Nau_An/app/src/main/java/com/example/cookingapp/ThemMonMoiActivity.java
@@ -7,14 +7,16 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.cookingapp.Fragment.ThemFragment;
 
 public class ThemMonMoiActivity extends AppCompatActivity {
-    ImageView imgAnhMon, imgAnhCachLam1, imgAnhCachLam2, imgAnhCachLam3;
+    ImageView imgAnhMon, imgAnhCachLam;
     Button btnThayDoiAnh, btnLuu, btnLenSong;
     Uri uri;
 
@@ -22,11 +24,11 @@ public class ThemMonMoiActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_them_mon_moi);
-        btnThayDoiAnh =  findViewById(R.id.btnThaydoiAnh);
+        btnThayDoiAnh = findViewById(R.id.btnThaydoiAnh);
         imgAnhMon = findViewById(R.id.imgAnhMon);
-        imgAnhCachLam1 = findViewById(R.id.imgAnhCachLam1);
-        imgAnhCachLam2 = findViewById(R.id.imgAnhCachLam2);
-        imgAnhCachLam3 = findViewById(R.id.imgAnhCachLam3);
+        imgAnhCachLam = findViewById(R.id.imgAnhCachLam);
+
+
 
 
 
@@ -34,43 +36,30 @@ public class ThemMonMoiActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(intent, 1);
+                startActivityForResult(intent, 3);
+
             }
         });
-        imgAnhCachLam1.setOnClickListener(new View.OnClickListener() {
+        imgAnhCachLam.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent1 = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(intent1, 2);
-            }
-        });
-        imgAnhCachLam2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent2 = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(intent2, 3);
-            }
-        });
-        imgAnhCachLam3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent3 = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(intent3, 4);
+                startActivityForResult(intent1, 3);
             }
         });
 
 
 
     }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK && data != null) {
+
             uri = data.getData();
             imgAnhMon.setImageURI(uri);
-            imgAnhCachLam1.setImageURI(uri);
-            imgAnhCachLam2.setImageURI(uri);
-            imgAnhCachLam3.setImageURI(uri);
-        }
+            imgAnhCachLam.setImageURI(uri);
+
+
     }
 }
