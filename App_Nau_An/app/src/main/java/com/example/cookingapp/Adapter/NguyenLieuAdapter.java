a60ffe52ac1244804e3af1740813677439719c76
Bao70kg <117332792+Bao70kg@users.noreply.github.com>
2022-11-21 13:24:46 +0700

    ag
    j

--- App_Nau_An/app/src/main/java/com/example/cookingapp/Adapter/NguyenLieuAdapter.java
+++ App_Nau_An/app/src/main/java/com/example/cookingapp/Adapter/NguyenLieuAdapter.java
@@ -2,45 +2,62 @@ package com.example.cookingapp.Adapter;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.DrawableUtils;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.cookingapp.R;
-import com.example.cookingapp.model.NguyenLieu;
+import com.example.cookingapp.model.Nguyenlieu;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
 public class NguyenLieuAdapter extends RecyclerView.Adapter<NguyenLieuAdapter.ViewHolder>{
+    private Context context;
+    private ArrayList<Nguyenlieu> list;
 
+    public NguyenLieuAdapter(Context context, ArrayList<Nguyenlieu> list) {
+        this.context = context;
+        this.list = list;
+    }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
-    }
 
+        LayoutInflater inflater  = ((Activity)context).getLayoutInflater();
+        View view =inflater.inflate(R.layout.item_nguyenlieu,parent,false);
+        return new ViewHolder(view);
+    }
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        holder.text.setText(""+list.get(position).getTenNL());
+        Picasso.with(context.getApplicationContext()).load(list.get(position).getAnhnguyenlieu()).placeholder(R.drawable.img).
+                error(R.drawable.img).
+                into(holder.img);
     }
-
     @Override
     public int getItemCount() {
-        return 0;
+        return list.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
+        ImageView img;
+        TextView text;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-
+        img = itemView.findViewById(R.id.img);
+        text = itemView.findViewById(R.id.text);
 
 
         }
--- App_Nau_An/app/src/main/java/com/example/cookingapp/Fragment/Home_Fragment.java
+++ App_Nau_An/app/src/main/java/com/example/cookingapp/Fragment/Home_Fragment.java
@@ -14,15 +14,37 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.cookingapp.Adapter.NguyenLieuAdapter;
 import com.example.cookingapp.R;
+import com.example.cookingapp.model.Nguyenlieu;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Home_Fragment extends Fragment {
-
+        RecyclerView recyclerviewHCK;
+        ArrayList<Nguyenlieu> list;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.home_fragment, container, false);
+        recyclerviewHCK = view.findViewById(R.id.recyclviewKCH);
+        list = new ArrayList<>();
+        list.add(new Nguyenlieu(1,"Thịt Hiếu","https://res.cloudinary.com/doluugxhe/image/upload/v1668265730/Cooking%20app/1_rjnyq8.jpg"));
+        list.add(new Nguyenlieu(2,"Thịt Bảo","https://res.cloudinary.com/doluugxhe/image/upload/v1668265730/Cooking%20app/1_rjnyq8.jpg"));
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        recyclerviewHCK.setLayoutManager(linearLayoutManager);
+        NguyenLieuAdapter adapter = new NguyenLieuAdapter(getContext(),list);
+        recyclerviewHCK.setAdapter(adapter);
+
         return view;
+
+
+
+
+
     }
 }
