60e252486ebf2ed84d72a34a380235c6c27d5f32
phong <phonghack02041999@gmail.com>
2022-11-28 16:02:35 +0700

    1.2.1

--- App_Nau_An/app/src/main/java/com/example/cookingapp/Fragment/ThemFragment.java
+++ App_Nau_An/app/src/main/java/com/example/cookingapp/Fragment/ThemFragment.java
@@ -10,17 +10,38 @@ import android.widget.Button;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.cookingapp.MonCuaToiActivity;
-import com.example.cookingapp.MonDaLuuActivity;
+import com.example.cookingapp.Adapter.DangMonAdapter;
 import com.example.cookingapp.R;
+import com.example.cookingapp.ThemMonMoiActivity;
+
+import java.util.ArrayList;
 
 public class ThemFragment extends Fragment {
-    Button btnMonCuaToi, btnMonDaLuu;
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.them_fragment, container, false);
+
+        Button btnThemMonMoi = view.findViewById(R.id.btnThemMonMoi);
+        RecyclerView recyclerMonDang = view.findViewById(R.id.recyclerMonDaDang);
+
+
+
+
+        btnThemMonMoi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), ThemMonMoiActivity.class));
+            }
+        });
+
+
+
 
         return view;
 
