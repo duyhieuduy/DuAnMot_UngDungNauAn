0c0f61275c1a5a8b90bc2f5ca9dd08d19cedfe90
duyhieuduy <hieu01648476512@gmail.com>
2022-11-29 12:43:35 +0700

    Update ThemFragment.java

--- App_Nau_An/app/src/main/java/com/example/cookingapp/Fragment/ThemFragment.java
+++ App_Nau_An/app/src/main/java/com/example/cookingapp/Fragment/ThemFragment.java
@@ -21,7 +21,6 @@ public class ThemFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.them_fragment, container, false);
-
         return view;
 
 
