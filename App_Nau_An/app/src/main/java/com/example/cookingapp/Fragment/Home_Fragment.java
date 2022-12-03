ace89eb59eebbdd0a40d247fd2e6f857bb5135e3
duyhieuduy <hieu01648476512@gmail.com>
2022-12-04 03:22:54 +0700

    a

--- App_Nau_An/app/src/main/java/com/example/cookingapp/Fragment/Home_Fragment.java
+++ App_Nau_An/app/src/main/java/com/example/cookingapp/Fragment/Home_Fragment.java
@@ -5,7 +5,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -31,6 +33,7 @@ public class Home_Fragment extends Fragment{
         RecyclerView recyclerviewnewHCK;
         ArrayList<NguyenLieu> list;
         ArrayList<NewNguyenLieu> listnew;
+
         dao daoz;
 
     NguyenLieuAdapter adapter;
@@ -39,7 +42,6 @@ public class Home_Fragment extends Fragment{
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.home_fragment, container, false);
 
-        RecyclerView recyclerMonAn = view.findViewById(R.id.recyclerMonAn);
         recyclerviewHCK = view.findViewById(R.id.recyclviewKCH);
         recyclerviewnewHCK = view.findViewById(R.id.recyclviewnewKCH);
         list = new ArrayList<>();
@@ -82,7 +84,15 @@ public class Home_Fragment extends Fragment{
                 fragmentTransaction.commit();
             }
         });
+//        mai lam
+//        list<loaimon> listloaimon = new ArrayList<>();
+//        ListView lvloaimon = view.findViewById(R.id.lvloaimon);
+//        ArrayAdapter arrayAdapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, listloaimon);
+//        lvloaimon.setAdapter(arrayAdapter);
+//
         return view;
+
+
     }
     private void loadDataNewnl(){
 
